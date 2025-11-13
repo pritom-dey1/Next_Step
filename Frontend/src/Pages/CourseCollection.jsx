c200a1d54301c123cc5b0104be454187c6b009b4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-14 00:09:26 +0600

    added

---
+++ Frontend/src/Pages/CourseCollection.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import img1 from "../assets/authentication1.png";
+import { Link } from "react-router-dom";
+
+const CourseCollection = () => {
+  return (
+    <section className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 px-5">
+        {/*  */}
+      <div className="bg-base-100 shadow-lg border rounded-xl p-3 mt-5 flex flex-col gap-4 hover:shadow-xl transition">
+        <img className="w-90 h-auto" src={img1} alt="" />
+        <h1 className="font-semibold text-2xl mt-2">
+          Forward Security Director
+        </h1>
+        <p>Bauch, Schuppe and Schulist Co.</p>
+        <Link>
+          <button className="btn btn-dash btn-info">Get Course</button>
+        </Link>
+      </div>
+      {/*  */}
+      <div className="bg-base-100 shadow-lg border rounded-xl p-3 mt-5 flex flex-col gap-4 hover:shadow-xl transition">
+        <img className="w-90 h-auto" src={img1} alt="" />
+        <h1 className="font-semibold text-2xl my-2">
+          Forward Security Director
+        </h1>
+        <p>Bauch, Schuppe and Schulist Co.</p>
+        <Link>
+          <button className="btn btn-dash btn-info">Get Course</button>
+        </Link>
+      </div>
+      {/*  */}
+      <div className="bg-base-100 shadow-lg border rounded-xl p-3 mt-5 flex flex-col gap-4 hover:shadow-xl transition">
+        <img className="w-90 h-auto" src={img1} alt="" />
+        <h1 className="font-semibold text-2xl my-2">
+          Forward Security Director
+        </h1>
+        <p>Bauch, Schuppe and Schulist Co.</p>
+        <Link>
+          <button className="btn btn-dash btn-info">Get Course</button>
+        </Link>
+      </div>
+      
+    </section>
+  );
+};
+
+export default CourseCollection;
--- Frontend/src/Pages/Resources.jsx
+++ Frontend/src/Pages/Resources.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import CourseCollection from "./CourseCollection";
 
 const Resources = () => {
   return (
@@ -7,20 +8,33 @@ const Resources = () => {
       <Helmet>
         <title>NextStep | Resources</title>
       </Helmet>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
-      <h1>This is Resources Page</h1>
+      <div className="h-50 bg-black flex items-center justify-center pt-20">
+        <h1 className="text-white  font-bold text-4xl text-center">
+          All Resources
+        </h1>
+      </div>
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="Courses Collection"
+          className="w-full pl-10 mt-5 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 mt-5  w-5 absolute left-3 top-3 text-gray-400"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M21 21l-4.35-4.35M5 11a6 6 0 1112 0 6 6 0 01-12 0z"
+          />
+        </svg>
+      </div>
+      <CourseCollection></CourseCollection>
     </div>
   );
 };
